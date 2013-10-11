8bc8d51e68774264b14f68f13e148d1e4e898752
tsux89 <tsux89@yahoo.com>
2013-10-11 16:58:00 +0900

    Create home.php

---
+++ home.php
@@ -0,0 +1,55 @@
+<style type="text/css"><!--
+body   {background-color:#ffffcc;
+}
+.title {color:#339966;
+        text-align:center;
+}
+.que   {color:#ffffff;
+        background-color:#339966;
+
+}
+.ans   {color:#000000;
+        background-color:#ccff99;
+}
+--></style>
+
+<?php
+
+require_once('UltimateOAuth.php');
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
+
+$un = '';
+$pw = '';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $un = htmlspecialchars($_POST["un"], ENT_QUOTES);
+    $pw = htmlspecialchars($_POST["pw"], ENT_QUOTES);
+} else {
+    echo "PLEASE ACCESS FROM THE FORM PAGE.";
+    exit(1);
+}
+
+$uo = new UltimateOAuth('RwYLhxGZpMqsWZENFVw', 'Jk80YVGqc7Iz1IDEjCI6x3ExMSBnGjzBAH6qHcWJlo');
+
+$res = $uo->directGetToken($un, $pw);
+if (isset($res->errors)) {
+	die($res->errors[0]->message);
+}
+
+echo '<table cellspacing="3" cellpadding="10" border="0" align="center">';
+echo '<tr><td width="220" class="que"<b>text</b></td><td size="380" class="ans"><form action="tweet.php" method="post"><textarea name="text" cols="50" rows="10"></textarea><br><input type="submit" value="Tweet"></td></tr>\n';
+
+$res = $uo->get('statuses/home_timeline', array('count' => '20'));
+
+foreach ( $res as $status ){
+	$status_id = $status->id_str;
+    $text = $status->text;
+    $user_id = $status->user->id_str;
+    $screen_name = $status->user->screen_name;
+    $name = $status->user->name;
+    echo '<tr><td width="220" class="que"><b>'.$screen_name.'<br>'.$name.'</b></td><td size="380" class="ans">'.$text.'</td></tr>\n';
+}
+
+echo '</table>';
+?>
